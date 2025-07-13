c206c3ac343fb7b5a22c67853f679be36e93959c
Alish-Shakya <alishshakya44@gmail.com>
2025-07-13 22:11:55 +0545

    cookies value stored

--- superGlobals/link.php
+++ superGlobals/link.php
@@ -1,18 +1,13 @@
 <?php
 
+setcookie("myfile", "gett.php", time() + (86400 * 7), "/");
+
 require  "gett.php";
-
-
 include "post.php";
-
-
 include "request.php";
-
 include "server.php";
-
 include "file.php";
-
 include "env.php";
-
 include "cookies.php";
+
 ?>
